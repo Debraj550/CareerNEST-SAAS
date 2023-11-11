b1399b424c74f07952cea93a63aadc4cbc0c8d6b
Debraj Dhar <debrajdhar100@gmail.com>
2023-11-11 18:00:33 +0530

    added authentication context

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -7,6 +7,7 @@ interface User {
   firstName: string;
   lastName: string;
   email: string;
+  isRecruiter: number;
   password: string;
   confirmpassword: string;
 }
@@ -16,20 +17,26 @@ const Signup = (props: Props) => {
     firstName: "",
     lastName: "",
     email: "",
+    isRecruiter: 0,
     password: "",
     confirmpassword: "",
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
+    const isSelectElement = e.target instanceof HTMLSelectElement;
+    // isRecruiter: 1 (recrutier), 0 (applicant)
     setUser((prevUser) => ({
       ...prevUser,
-      [name]: value,
+      [name]: isSelectElement ? Number(value) : value,
     }));
   };
 
+  //console.log(user);
   return (
-    <div className="bg-slate-100  p-4 rounded-xl w-3/12">
+    <div className="bg-slate-100  p-4 rounded-xl w-3/12 flex flex-col gap-1">
       <div className="p-4 text-center border-b-2 mb-4">
         <h1 className="font-bold text-gray-600 text-lg">Create your account</h1>
       </div>
@@ -64,6 +71,21 @@ const Signup = (props: Props) => {
           value={user.email}
           onChange={handleChange}
         ></input>
+      </div>
+      <div className="">
+        <h1 className="text-sm font-semibold py-2">
+          Are you an seeking Jobs or looking for candidates?{" "}
+        </h1>
+        <select
+          className="w-full px-2 py-1 border-gray-300 border"
+          name="isRecruiter"
+          id="isRecruiter"
+          value={user.isRecruiter ? "1" : "0"}
+          onChange={handleChange}
+        >
+          <option value="0">Exploring job opportunities</option>
+          <option value="1">Seeking candidates for hiring positions</option>
+        </select>
       </div>
       <div>
         <h1 className="text-sm font-semibold py-2">Password</h1>
---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, createContext, ReactNode } from "react";
+
+interface AuthContextProps {
+  // Define your context properties here
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  // Your provider logic goes here
+
+  return (
+    <AuthContext.Provider
+      value={
+        {
+          /* Provide your context values */
+        }
+      }
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
