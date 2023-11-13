f2204a4eb05487948315dbc0a70e2909ad0da262
ras5014 <ramanandasamantaray@gmail.com>
2023-11-13 20:19:07 +0530

    postgres bug fixed

--- server/server.js
+++ server/server.js
@@ -4,21 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import knex from "knex";
 import bcrypt from "bcryptjs";
+import dbConfig from "./utils/dbconnection.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 // Database configuration
-const db = knex({
-  client: "pg",
-  connection: {
-    // host: process.env.postgres_HOST,
-    // port: process.env.postgres_PORT,
-    // user: process.env.postgres_USER,
-    // password: process.env.postgres_PASSWORD,
-    // database: process.env.postgres_DATABASE,
-    connectionString: process.env.postgres_URL + "?sslmode=require",
-  },
-});
+const db = knex(dbConfig.development);
 
 const app = express();
 
@@ -28,13 +19,13 @@ app.get("/", (req, res) => {
 
 app.listen(process.env.PORT, async () => {
   await db
-    .raw("SELECT * 1")
+    .raw("SELECT 1")
     .then(() => {
       console.log("Postgres Connected");
     })
     .catch((e) => {
       console.log("Postgres Not Connected");
-      console.log(e.message);
+      console.log(e);
     });
   console.log(`Server started on http://localhost:${process.env.PORT}/`);
 });
