ee3f94c25f1d1a1efbb1cc73852691d7926e45f0
Debraj Dhar <debrajdhar100@gmail.com>
2023-11-13 20:08:21 +0530

    added server ssl authorization check

--- server/server.js
+++ server/server.js
@@ -1,24 +1,14 @@
-// All the Imports
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import knex from "knex";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
+import dbConfig from "./dbconnection.js";
+
 dotenv.config();
 
-// Database configuration
-const db = knex({
-  client: "pg",
-  connection: {
-    // host: process.env.postgres_HOST,
-    // port: process.env.postgres_PORT,
-    // user: process.env.postgres_USER,
-    // password: process.env.postgres_PASSWORD,
-    // database: process.env.postgres_DATABASE,
-    connectionString: process.env.postgres_URL + "?sslmode=require",
-  },
-});
+const db = knex(dbConfig.development);
 
 const app = express();
 
@@ -27,14 +17,12 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT, async () => {
-  await db
-    .raw("SELECT * 1")
-    .then(() => {
-      console.log("Postgres Connected");
-    })
-    .catch((e) => {
-      console.log("Postgres Not Connected");
-      console.log(e.message);
-    });
+  try {
+    await db.raw("SELECT 1");
+    console.log("Postgres Connected");
+  } catch (e) {
+    console.log("Postgres Not Connected");
+    console.log(e.message);
+  }
   console.log(`Server started on http://localhost:${process.env.PORT}/`);
 });
