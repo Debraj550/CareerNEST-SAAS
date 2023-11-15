83b8d6ffe7c9dddfaaf7b61e2b42bcfc888a0bf2
ras5014 <ramanandasamantaray@gmail.com>
2023-11-15 21:07:30 +0530

    Backend Updated

---
+++ server/Authentication_Service/utils/dbconnection.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbConfig = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.postgres_HOST,
+      user: process.env.postgres_USER,
+      password: process.env.postgres_PASSWORD,
+      database: process.env.postgres_DATABASE,
+      port: process.env.postgres_PORT,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+  // ... other environments
+};
+
+export default dbConfig;
