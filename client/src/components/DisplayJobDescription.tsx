8c54aa9ba970a6767520cab27b9631265ddc0e24
Debraj Dhar <debrajdhar100@gmail.com>
2023-12-05 19:43:16 +0530

    added jobdescription page changes

--- client/src/components/DisplayJobDescription.tsx
+++ client/src/components/DisplayJobDescription.tsx
@@ -47,8 +47,10 @@ const DisplayJobDescription = ({ job }: DisplayJobDescriptionProps) => {
     <div className="border-2 shadow-md min-h-[750px] max-h-screen mx-4 px-6 py-2 overflow-y-scroll relative rounded-lg">
       <div className="mb-4">
         <h1 className="text-2xl font-bold ">{role.toUpperCase()}</h1>
-        <p className="text-lg text-blue-600 font-bold">{country}</p>
-        <p>{city}</p>
+        <p className="text-lg text-blue-600 font-bold">
+          {city},{country}
+        </p>
+
         <p>Posted By - {posted_by}</p>
         <div className="my-2 w-full ">
           <h1 className="font-bold text-lg border-t border-b py-2 rounded-md px-2">
